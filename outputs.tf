1b372583431fce8e520370ae0c69fa8e6c1561d0
Joeri Voet <2750496+jwavoet@users.noreply.github.com>
2024-04-19 15:41:40 +0200

    Replace * with [0] because deprecated according to tflint.

--- outputs.tf
+++ outputs.tf
@@ -1,28 +1,28 @@
 output "mfa_policy_arn" {
   description = "MFA Policy arn."
-  value       = element(concat(aws_iam_policy.mfa.*.arn, [""]), 0)
+  value       = element(concat(aws_iam_policy.mfa[0].arn, [""]), 0)
 }
 
 output "trail_arn" {
   description = "Cloud trail arn."
-  value       = element(concat(aws_cloudtrail.cloudtrail.*.arn, [""]), 0)
+  value       = element(concat(aws_cloudtrail.cloudtrail[0].arn, [""]), 0)
 }
 
 output "cloudwatch_log_group_arn" {
   description = "Cloud trail arn."
-  value       = element(concat(aws_cloudwatch_log_group.log_group.*.arn, [""]), 0)
+  value       = element(concat(aws_cloudwatch_log_group.log_group[0].arn, [""]), 0)
 }
 
 output "monitor_readonly_user_arn" {
   description = "ARN for the monitor readonly user"
-  value       = element(concat(aws_iam_user.monitor_readonly_user.*.arn, [""]), 0)
+  value       = element(concat(aws_iam_user.monitor_readonly_user[0].arn, [""]), 0)
 }
 
 output "monitor_readonly_user_access_key_id" {
   description = "Access key id for the monitor readonly user"
   value = element(
     concat(
-      aws_iam_access_key.monitor_readonly_user_access_key.*.id,
+      aws_iam_access_key.monitor_readonly_user_access_key[0].id,
       [""],
     ),
     0,
@@ -33,7 +33,7 @@ output "monitor_readonly_user_secret_access_key" {
   description = "Secret access key for the monitor readonly user"
   value = element(
     concat(
-      aws_iam_access_key.monitor_readonly_user_access_key.*.secret,
+      aws_iam_access_key.monitor_readonly_user_access_key[0].secret,
       [""],
     ),
     0,
